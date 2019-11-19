18b56abb9b2474e28e2e67eee914ed8dae1b0604
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2019-11-19 12:17:07 -0500

    create basic template page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InstructionsComponent} from './instructions/instructions.component';
+import {IntroComponent} from './intro/intro.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'instructions',
+    component: InstructionsComponent
+  },
+  {
+    path: 'intro',
+    component: IntroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InstructionsComponent } from './instructions/instructions.component';
+import { IntroComponent } from './intro/intro.component';
+import {MatCardModule, MatStepperModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InstructionsComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
