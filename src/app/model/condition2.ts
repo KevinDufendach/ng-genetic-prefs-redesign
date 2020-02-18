1a2344ad255575ab09019535a26890b6e2bca5e2
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2020-02-18 09:17:48 -0500

    create condition interface consistent with json objects

---
+++ src/app/model/condition2.ts
@@ -0,0 +1,17 @@
+export interface Condition2 {
+  genes: string | null;
+  carrierPanel: string | null;
+  inheritance: string;
+  conditions: string;
+  category: string;
+  description: string;
+  preventable: boolean;
+  preventableText: string;
+  curable: boolean;
+  curableText: string;
+  adultOnset: boolean;
+  adultOnsetText: string;
+  carrier: boolean;
+  carrierText: string;
+  resources: string;
+}
