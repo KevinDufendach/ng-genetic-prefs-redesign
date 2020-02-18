7a748b4a180649535aa1ba8512656930912dfa9a
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2020-02-18 09:43:42 -0500

    import conditions_list.json for data in condition-table

--- src/app/condition-table/condition-table.component.ts
+++ src/app/condition-table/condition-table.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {CONDITION_DATA} from '../model/condition';
+// import {CONDITION_DATA} from '../model/condition';
 import {SurveyService} from '../survey.service';
+
+// @ts-ignore
+import * as data from '../model/condition_list.json';
+import {Condition2} from '../model/condition2';
 
 @Component({
   selector: 'app-condition-table',
@@ -9,11 +13,15 @@ import {SurveyService} from '../survey.service';
 })
 export class ConditionTableComponent implements OnInit {
   displayedColumns: string[] = ['preventable', 'name'];
-  dataSource = CONDITION_DATA;
+  // dataSource = CONDITION_DATA;
 
-  constructor(public survey: SurveyService) { }
+  conditions: Condition2[] = (data as any).default;
+
+  constructor(public survey: SurveyService) {
+  }
 
   ngOnInit(): void {
+    console.log(this.conditions);
   }
 
 }
