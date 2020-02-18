b7a2d0739ffd3c167c80d275ee34c6afbab03d55
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2020-02-18 11:31:26 -0500

    create getters and setters for survey options

--- src/app/survey.service.ts
+++ src/app/survey.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Selections} from './model/selections';
 
 @Injectable({
@@ -12,5 +12,54 @@ export class SurveyService {
     carrierStatus: -1,
   };
 
-  constructor() { }
+  constructor() {
+  }
+
+  get curability() {
+    if (this.selections.curability < 0) {
+      return null;
+    }
+
+    return (this.selections.curability === 1);
+  }
+
+  set curability(val: boolean) {
+    this.selections.curability = (val ? 1 : 0);
+  }
+
+  get preventability() {
+    if (this.selections.preventability < 0) {
+      return null;
+    }
+
+    return (this.selections.preventability === 1);
+  }
+
+  set preventability(val: boolean) {
+    this.selections.preventability = (val ? 1 : 0);
+  }
+
+  get adultOnset() {
+    if (this.selections.adultOnset < 0) {
+      return null;
+    }
+
+    return (this.selections.adultOnset === 1);
+  }
+
+  set adultOnset(val: boolean) {
+    this.selections.adultOnset = (val ? 1 : 0);
+  }
+
+  get carrierStatus() {
+    if (this.selections.carrierStatus < 0) {
+      return null;
+    }
+
+    return (this.selections.carrierStatus === 1);
+  }
+
+  set carrierStatus(val: boolean) {
+    this.selections.carrierStatus = (val ? 1 : 0);
+  }
 }
