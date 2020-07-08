2795533bdb15b9f56cecc59911b7a50b3f2460c5
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2020-07-08 10:13:25 -0400

    implement stepper for content

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -9,10 +9,17 @@ import {CONDITION_DATA} from '../model/condition';
 export class IntroComponent implements OnInit {
   preventabilitySelection = -1;
   notSureAllowed = false;
+  step = 1;
+  stepCount = 3;
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  cycleStep() {
+    this.step++;
+    if (this.step >= this.stepCount) { this.step = 0; }
   }
 }
