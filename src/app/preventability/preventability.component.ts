0a3a0b8a04e1f2da16961a0a7b892673c31b6296
Dufendach, Kevin <Kevin.Dufendach@cchmc.org>
2020-08-27 13:06:14 -0400

    convert preventability to new format

--- src/app/preventability/preventability.component.ts
+++ src/app/preventability/preventability.component.ts
@@ -1,19 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SurveyService} from '../survey.service';
 import {SURVEY_STEP} from '../condition-manager.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { stepAnimations } from '../step-animations';
 
 @Component({
   selector: 'app-preventability',
   templateUrl: './preventability.component.html',
-  styleUrls: ['./preventability.component.scss']
+  styleUrls: ['./preventability.component.scss', '../../survey.scss'],
+  animations: [stepAnimations.verticalStepTransition]
 })
-export class PreventabilityComponent {
+export class PreventabilityComponent implements OnInit {
   surveySteps = SURVEY_STEP;
   notSureAllowed = false;
+  stepCount = 2;
+  page = 0;
 
   testRadioResponse: boolean | null = null;
 
-  constructor(public survey: SurveyService) { }
+  constructor(
+    public survey: SurveyService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   get value(): number {
     return this.survey.getPreventability();
@@ -21,5 +30,42 @@ export class PreventabilityComponent {
 
   set value(val: number) {
     this.survey.setPreventability(val, {context: 'Preventability Step'});
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.page = +params.page || 0;
+    });
+
+    this.updateRouteQueryParameters();
+  }
+
+  navigate(direction = 1) {
+    this.page = this.page + direction;
+
+    if (this.page < 0) {
+      this.router.navigate(['/treatability']);
+    } else if (this.page >= this.stepCount) {
+      this.router.navigate(['/adult-onset']);
+    } else {
+      this.updateRouteQueryParameters();
+    }
+  }
+
+  updateRouteQueryParameters() {
+    this.router.navigate(
+      [],
+      {
+        relativeTo: this.route,
+        queryParams: {page: this.page},
+        queryParamsHandling: 'merge'
+      }
+    );
+  }
+
+  getDirection(stepIndex: number) {
+    if (stepIndex === this.page) { return 'current'; }
+
+    return (stepIndex < this.page ? 'previous' : 'next');
   }
 }
