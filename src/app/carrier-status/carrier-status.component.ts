6a8e34907e507bf81a6df487cf8e2d01c50014d9
Dufendach, Kevin <Kevin.Dufendach@cchmc.org>
2020-08-27 13:38:32 -0400

    update carrier-status component to new XD design

--- src/app/carrier-status/carrier-status.component.ts
+++ src/app/carrier-status/carrier-status.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {SurveyService} from '../survey.service';
-import {SURVEY_STEP} from '../condition-manager.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { stepAnimations } from '../step-animations';
 
 @Component({
   selector: 'app-carrier-status',
   templateUrl: './carrier-status.component.html',
-  styleUrls: ['./carrier-status.component.scss']
+  styleUrls: ['./carrier-status.component.scss', '../../survey.scss'],
+  animations: [stepAnimations.verticalStepTransition]
 })
-export class CarrierStatusComponent {
-  surveySteps = SURVEY_STEP;
+export class CarrierStatusComponent implements OnInit {
   notSureAllowed = false;
+  stepCount = 2;
+  page = 0;
 
-  constructor(public survey: SurveyService) { }
+  constructor(
+    public survey: SurveyService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   get value(): number {
     return this.survey.getCarrierStatus();
@@ -19,5 +26,42 @@ export class CarrierStatusComponent {
 
   set value(val: number) {
     this.survey.setCarrierStatus(val, {context: 'Carrier Status Step'});
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.page = +params.page || 0;
+    });
+
+    this.updateRouteQueryParameters();
+  }
+
+  navigate(direction = 1) {
+    this.page = this.page + direction;
+
+    if (this.page < 0) {
+      this.router.navigate(['/adult-onset']);
+    } else if (this.page >= this.stepCount) {
+      this.router.navigate(['/review']);
+    } else {
+      this.updateRouteQueryParameters();
+    }
+  }
+
+  updateRouteQueryParameters() {
+    this.router.navigate(
+      [],
+      {
+        relativeTo: this.route,
+        queryParams: {page: this.page},
+        queryParamsHandling: 'merge'
+      }
+    );
+  }
+
+  getDirection(stepIndex: number) {
+    if (stepIndex === this.page) { return 'current'; }
+
+    return (stepIndex < this.page ? 'previous' : 'next');
   }
 }
