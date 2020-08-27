0c5e2240c67337c64685e30d54bf50750d9ee133
Dufendach, Kevin <Kevin.Dufendach@cchmc.org>
2020-08-27 13:29:46 -0400

    convert adult onset to new format

--- src/app/adult-onset/adult-onset.component.ts
+++ src/app/adult-onset/adult-onset.component.ts
@@ -1,17 +1,24 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {SurveyService} from '../survey.service';
-import {SURVEY_STEP} from '../condition-manager.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { stepAnimations } from '../step-animations';
 
 @Component({
   selector: 'app-adult-onset',
   templateUrl: './adult-onset.component.html',
-  styleUrls: ['./adult-onset.component.scss']
+  styleUrls: ['./adult-onset.component.scss' , '../../survey.scss'],
+  animations: [stepAnimations.verticalStepTransition]
 })
-export class AdultOnsetComponent {
-  surveySteps = SURVEY_STEP;
+export class AdultOnsetComponent implements OnInit {
   notSureAllowed = false;
+  stepCount = 2;
+  page = 0;
 
-  constructor(public survey: SurveyService) { }
+  constructor(
+    public survey: SurveyService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   get value(): number {
     return this.survey.getAdultOnset();
@@ -19,5 +26,42 @@ export class AdultOnsetComponent {
 
   set value(val: number) {
     this.survey.setAdultOnset(val, {context: 'Curability Step'});
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.page = +params.page || 0;
+    });
+
+    this.updateRouteQueryParameters();
+  }
+
+  navigate(direction = 1) {
+    this.page = this.page + direction;
+
+    if (this.page < 0) {
+      this.router.navigate(['/preventability']);
+    } else if (this.page >= this.stepCount) {
+      this.router.navigate(['/carrier-status']);
+    } else {
+      this.updateRouteQueryParameters();
+    }
+  }
+
+  updateRouteQueryParameters() {
+    this.router.navigate(
+      [],
+      {
+        relativeTo: this.route,
+        queryParams: {page: this.page},
+        queryParamsHandling: 'merge'
+      }
+    );
+  }
+
+  getDirection(stepIndex: number) {
+    if (stepIndex === this.page) { return 'current'; }
+
+    return (stepIndex < this.page ? 'previous' : 'next');
   }
 }
